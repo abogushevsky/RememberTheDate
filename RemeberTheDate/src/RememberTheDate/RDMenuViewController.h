4b34ede8e7c9976b4419ca9067b1883eb950198c
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2014-06-15 16:38:36 +0400

    Navigation controller added

---
+++ RemeberTheDate/src/RememberTheDate/RDMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  RDMenuViewController.h
+//  RememberTheDate
+//
+//  Created by Anton Bogushevsky on 15.06.14.
+//  Copyright (c) 2014 Anton Bogushevsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDMenuViewController : UIViewController
+
+@end
