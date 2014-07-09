e6f17973ec237171a9efe5bce5dc8087f952ced8
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2014-07-09 22:33:51 +0400

    RDFieldView added

---
+++ RemeberTheDate/src/RememberTheDate/RDFieldView.h
@@ -0,0 +1,13 @@
+//
+//  RDFieldView.h
+//  RememberTheDate
+//
+//  Created by Anton Bogushevsky on 09.07.14.
+//  Copyright (c) 2014 Anton Bogushevsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDFieldView : UIView
+
+@end
